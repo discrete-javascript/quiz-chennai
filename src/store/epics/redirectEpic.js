6e641938f2390ed9b082996284be54518bd4276d
Rajesh W <wraejesh@gmail.com>
2020-05-14 22:53:01 +0530

    Fixed redirectEpic error

--- src/store/epics/redirectEpic.js
+++ src/store/epics/redirectEpic.js
@@ -12,11 +12,17 @@ import { Observable, of, interval } from 'rxjs';
 import { ofType } from 'redux-observable';
 import { REDIRECT } from '../actions/constants';
 import history from '../../utils/history';
-import { loginFail } from '../actions/authActions';
+import { loginFail, loginSuccess } from '../actions/authActions';
 
 // const source = interval(1000);
-const redirectEpic = (action$) => {
-    const redirect = action$.pipe(ofType(REDIRECT).subscribe(val => console.log(val));;
-};
+const redirectEpic = (action$) =>
+  action$.pipe(
+    ofType(REDIRECT),
+    // Instead of map we have to write some logic to redirect to given URL
+    map(({ payload }) => {
+      console.log(payload);
+      return loginSuccess('something');
+    })
+  );
 
 export default redirectEpic;
