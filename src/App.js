79a07300ac473aecee2ee2d85dc5e788601bbfd9
Rajesh W <wraejesh@gmail.com>
2020-05-16 23:16:36 +0530

    Changed root routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
 import ScoreCard from './components/score-card/score.card';
 import SignIn from './components/sign-in/sign.in';
 import SignUp from './components/sign-up/sign.up';
@@ -20,20 +21,29 @@ class App extends React.PureComponent {
             justify="flex-start"
             alignItems="center"
           >
-            {/* <Switch> */}
-            <Route exact path={['/mobile/quiz', '/sign-in']}>
-              <SignIn />
-            </Route>
-            <Route exact path="/sign-up">
-              <SignUp />
-            </Route>
-            <Route exact path="/questions">
-              <QuizContainer />
-            </Route>
-            <Route exact path="/score-card">
-              <ScoreCard />
-            </Route>
-            {/* </Switch> */}
+            <Switch>
+              <Route exact path="/">
+                Welcome
+                <Link to="/sign-in" variant="body2">
+                  {'Please sign in to get started'}
+                </Link>
+                <Link to="/sign-up" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Route>
+              <Route exact path="/sign-in">
+                <SignIn />
+              </Route>
+              <Route exact path="/sign-up">
+                <SignUp />
+              </Route>
+              <Route exact path="/questions">
+                <QuizContainer />
+              </Route>
+              <Route exact path="/score-card">
+                <ScoreCard />
+              </Route>
+            </Switch>
           </Grid>
         </Container>
       </div>
